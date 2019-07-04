847926a87843becf2413235429b6b1c14cd5d1d5
wbhm <wbhm@mac.com>
2019-07-04 10:20:53 -0500

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { CustomMaterialModule } from './core/material.module';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { PageComponent } from './components/page/page.component';
+import { PageContentComponent } from './components/page-content/page-content.component';
+import { LeftNavComponent } from './components/left-nav/left-nav.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    PageComponent,
+    PageContentComponent,
+    LeftNavComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CustomMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
